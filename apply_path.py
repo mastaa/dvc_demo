8d54ac79a7d1c06c16ef165fe6e659c5f685cfb6
Jacek stelmaski <jacek.stelmaski@gmail.com>
2022-03-30 23:22:15 +0200

    modified service.py

--- apply_path.py
+++ apply_path.py
@@ -1,9 +1,13 @@
-
+import argparse
 import difflib
+from typing import Union, Any
+
 import dvc.api
 
 import pandas as pd
 from git import Repo
+from pandas import Series, DataFrame
+from pandas.io.parsers import TextFileReader
 
 
 def diff_version(f1: object, f2: object, f3: object) -> object:
@@ -13,10 +17,14 @@ def diff_version(f1: object, f2: object, f3: object) -> object:
     # print(delta)
 
 
-def export(file_name: str, sep: str):
+def export_to_json(file_name: str, sep: str):
     csv_file = pd.DataFrame(pd.read_csv(file_name, sep=sep, header=0, index_col=False))
     csv_file.to_json(file_name + "json", orient="records", date_format="epoch", double_precision=10, force_ascii=False,
                      date_unit="ms", default_handler=None)
+
+
+def export_to_excel(file_name: str, sep: str):
+    csv_file = pd.DataFrame(pd.read_csv(file_name, sep=sep, header=0, index_col=False))
     csv_file.to_excel(file_name + ".xlsx", sheet_name='export', index=False)
 
 
@@ -38,28 +46,40 @@ def create_dump_tag_version(dump: str, version: str, storage: str):
     file_write = open(file_name, mode='w', encoding='utf-8')
     for line in file:
         file_write.write(line)
-
+    repo.git.add(u=True)
     repo.commit()
     repo.create_tag(version, message='Create new version of dump "{0}"'.format(version))
+    repo.git.push()
+    # if __name__ == "__main__":
+    #     parser = argparse.ArgumentParser()
+    #     parser.add_argument('v1', help='rev1 ')
+    #     parser.add_argument('v2', help='rev2')
+    #     parser.add_argument('repo', help='repo url')
+    #     parser.add_argument('dump', help='dump')
+    #     parser.add_argument('storage', help='file storage')
+    #     args = parser.parse_args()
+    # f1 = dvc.api.open('data/' + str(args.dump), str(args.repo), str(args.v1), str(args.storage), mode='r',
+    #                   encoding='utf-8')
+    # f2 = dvc.api.open('data/' + str(args.dump), str(args.repo), str(args.v2), str(args.storage), mode='r',
+    #                   encoding='utf-8')
+    # f3 = open('data/apply.path', mode='w', encoding="utf-8")
+
+    #     diff_version(f1, f2, f3)
 
 
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('v1', help='rev1 ')
-#     parser.add_argument('v2', help='rev2')
-#     parser.add_argument('repo', help='repo url')
-#     parser.add_argument('dump', help='dump')
-#     parser.add_argument('storage', help='file storage')
-#     args = parser.parse_args()
-#     f1 = dvc.api.open('data/' + str(args.dump), str(args.repo), str(args.v1), str(args.storage), mode='r',
-#                       encoding='utf-8')
-#     f2 = dvc.api.open('data/' + str(args.dump), str(args.repo), str(args.v2), str(args.storage), mode='r',
-#                       encoding='utf-8')
-#     f3 = open('data/apply.path', mode='w', encoding="utf-8")
-#     diff_version(f1, f2, f3)
-
-create_dump_tag_version('KP_CENA_MOD_APR.DAT', 'V1.4', 'myremote')
-
-# get_dump_version('KP_CENA_MOD_APR.DAT', 'C:/dev/projects/apply_patch/', 'v1.0', 'myremote')
+# columns = ["id", "id_cennika", "cena_netto", "dlugosc_loj", "id_model_apr", "data_od", "data_do", "id_taryfy",
+#            "id_kwota_zobow", "id_kwota_rachunku", "vat_id", "subsyd_apr", "kierunek_migracji", "id_grupa_apr",
+#            "metoda_platnosci", "operator"]
+# with dvc.api.open('./data/KP_CENA_MOD_APR.DAT', "./", "8a1770b", "master", mode='r', encoding='utf-8') as f1:
+#     data_frame1 = pd.DataFrame(pd.read_csv(f1, sep='\u007F', names=columns))
+#
+# with dvc.api.open('./data/KP_CENA_MOD_APR.DAT', "./", "V1.0", "master", mode='r', encoding='utf-8') as f2:
+#     data_frame2 = pd.DataFrame(pd.read_csv(f2, sep='\u007F', names=columns))
+#
+# # data_frame1.to_csv('./data/data_frame1.DAT', sep='\u007F', header=False, index=False)
+# # data_frame2.to_csv('./data/data_frame2.DAT', sep='\u007F', header=False, index=False)
+# data_frame3 = pd.concat([data_frame1, data_frame2]).drop_duplicates(keep=False)
+# compression_opts = dict(method='zip', archive_name='apply_patch.DAT')
+# data_frame3.to_csv('data/apply_patch.zip', sep='\u007F', header=False, index=False, compression=compression_opts)
 
 # apply_path('KP_CENA_MOD_APR.DAT', 'KP_CENA_MOD_APR_V1.0.DAT', 'KP_CENA_MOD_APR_DIFF.DAT')
--- service.py
+++ service.py
@@ -1,12 +1,19 @@
-import os
-from flask import Flask, render_template, request
+from http.client import HTTPException
+from io import BytesIO
 
+import dvc.api
+import pandas as pd
+from flask import Flask, render_template, request, send_file, json
 
+from create_dump import export_table_db
 
 UPLOAD_FOLDER = '/uploads'
 app = Flask(__name__, template_folder='./templates')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.debug = True
+columns = ["id", "id_cennika", "cena_netto", "dlugosc_loj", "id_model_apr", "data_od", "data_do", "id_taryfy",
+           "id_kwota_zobow", "id_kwota_rachunku", "vat_id", "subsyd_apr", "kierunek_migracji", "id_grupa_apr",
+           "metoda_platnosci", "operator"]
 
 
 @app.route("/")
@@ -16,13 +23,14 @@ def index():
 
 @app.route("/import")
 def import_dump():
-    #import_db('./data/dump.csv')
+    # import_db('./data/dump.csv')
     return "import dump"
 
 
 @app.route("/create_dump")
 def create_dump():
-    #    export_table_db()
+    export_table_db('localhost', '5432', 'postgres', 'postgres', 'postgres', 'dvc_db', 'price', '\u007F',
+                    'KP_CENA_MOD_APR.DAT')
     return "create dump"
 
 
@@ -35,5 +43,69 @@ def upload():
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save('./uploads/'+f.filename)
+        f.save('./uploads/' + f.filename)
         return 'file uploaded successfully'
+
+
+@app.route('/version/<rev1>/diff/<rev2>/store/<storage>', methods=['GET'])
+def delta(rev1: str, rev2: str, storage: str):
+    with dvc.api.open('./data/KP_CENA_MOD_APR.DAT', "./", rev1, storage, mode='r', encoding='utf-8') as f1:
+        data_frame1 = pd.DataFrame(pd.read_csv(f1, sep='\u007F', names=columns))
+
+    with dvc.api.open('./data/KP_CENA_MOD_APR.DAT', "./", rev2, storage, mode='r', encoding='utf-8') as f2:
+        data_frame2 = pd.DataFrame(pd.read_csv(f2, sep='\u007F', names=columns))
+
+    data_frame3 = pd.concat([data_frame1, data_frame2]).drop_duplicates(keep=False)
+    compression_opts = dict(method='zip', archive_name='apply_patch.DAT')
+    data_frame3.to_csv('./data/apply_patch.zip', sep='\u007F', header=False, index=False, compression=compression_opts)
+
+    path = "./data/apply_patch.zip"
+    return send_file(path, as_attachment=True)
+
+
+@app.route('/version/<repo>/<version>/store/<store>/dump/<dump>',
+           defaults={'repo': './', 'dump': 'KP_CENA_MOD_APR.DAT'})
+@app.route('/version/<version>/store/<store>', defaults={'repo': './', 'dump': 'KP_CENA_MOD_APR.DAT'})
+def version(version: str, store: str, dump: str, repo):
+    tmp_file = dump.split('.')[0]
+    file_name: str = f"./data/{tmp_file}_{version}.DAT".format(tmp_file, version)
+    with dvc.api.open('./data/' + dump, repo, version.upper(), store, mode='r', encoding='utf-8') as fd:
+        data_frame = pd.DataFrame(pd.read_csv(fd, sep='\u007F', names=columns))
+        compression_opts = dict(method='zip', archive_name=f"{tmp_file}_{version}.DAT".format(tmp_file, version))
+        data_frame.to_csv('./data/download.zip', sep='\u007F', header=False, index=False, compression=compression_opts)
+
+        with open('./data/download.zip', 'rb') as fh:
+            buf = BytesIO(fh.read())
+
+        return send_file(buf, as_attachment=True, mimetype="text/plain",
+                         attachment_filename="download.zip".format(tmp_file, version))
+
+
+@app.errorhandler(dvc.exceptions.FileMissingError)
+def handle_bad_request(e):
+    return 'File not found!', 404
+
+
+@app.errorhandler(dvc.exceptions.PathMissingError)
+def handle_bad_request(e):
+    return 'File not found!', 404
+
+
+@app.errorhandler(dvc.scm.base.RevError)
+def handle_bad_request(e):
+    return 'File not found!', 404
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
